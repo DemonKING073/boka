4d51cd27ed8af4b9d04f5ab9b85f329a1e760270
DemonKING073 <legendsnever7die@gmail.com>
2021-05-08 07:49:56 +0545

    about location completed

---
+++ script.js
@@ -0,0 +1,15 @@
+// //home auto image slide 
+// var counter = 1;
+// setInterval(function(){
+//     document.getElementById('radio'+counter).checked=true;
+//     counter++;
+//     if(counter>3){
+//         counter=1;
+//     }
+// },5000);
+
+// //map integration
+
+
+
+
