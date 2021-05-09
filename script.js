fadf64dd59f0fa3fbd20e81be85fe0b6050c4d46
DemonKING073 <legendsnever7die@gmail.com>
2021-05-09 22:00:38 +0545

    almost complted

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
-//home auto image slide 
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio'+counter).checked=true;
-    counter++;
-    if(counter>3){
-        counter=1;
-    }
-},5000);
-
 // //map integration
+        let map;
+        function initMap() {
+            var location = {lat:-33.96716,lng:151.10666};
+            var map = new google.maps.Map(document.getElementById("map"),{
+                zoom:18,
+                center: location
+            });
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map,
+            })
+        }
+
+
+//Login Authentication
+var users = [
+    {
+        user:"admin",
+        password:'admin123'
+    },
+    {
+        user:"nirdesan",
+        password:'kera'
+    }
+]
+function login() {
+    var user = document.getElementById('user').value;
+    var logged = false;
+    var password = document.getElementById('password').value;
+    for(i=0; i<users.length;i++){
+        if(user==users[i].user && password ==users[i].password){
+            logged=true;
+        }
+    }
+    if(logged){
+        console.log('user loged')
+        alert('Logged Successfully');
+        window.location.href='./index.html'
+    }else{
+        alert('Login Failed!')
+    }
+}
+
+
+function addCart(a){
+    console.log(a)
+}
 
 
 
